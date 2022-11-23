727336c56958f4fe9f3f00ba64ca32f1a1f83061
Taiwo Sodiq <alhassansahih@gmail.com>
2022-11-24 00:48:37 +0100

    work preview

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('nav');
 const workSection = document.querySelector('.my-work');
+const projects = document.querySelectorAll('.work');
+const previewPopup = document.querySelector(".preview-container");
+const closePopup =  document.querySelector('.close-popup')
 
 menu.addEventListener('click', () => {
     nav.classList.toggle('toggle-nav');
@@ -14,7 +17,27 @@ function bgTransition() {
         workSection.classList.add('transition-bg')
     }else {
         workSection.classList.remove('transition-bg')
-
     }
 }
 
+const previewProject = () => {
+   projects.forEach((project, i) => {
+    project.addEventListener('click', function () {
+        const popup1 = previewPopup.children[0];
+        const popup2 = previewPopup.children[1];
+        previewPopup.classList.add("active") ;
+        popup1.classList.add("active") ;
+        popup2.classList.add("active") ;
+    })
+   }) 
+}
+
+closePopup.addEventListener('click', () => {
+    const popup1 = previewPopup.children[0];
+    const popup2 = previewPopup.children[1];
+    previewPopup.classList.remove("active") ;
+    popup1.classList.remove("active") ;
+    popup2.classList.remove("active") ;  
+})
+
+previewProject();
