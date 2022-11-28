c929a10453d8b320c611f3ca1882d752ca918170
Taiwo Sodiq <alhassansahih@gmail.com>
2022-11-28 10:14:20 +0100

    tabed content

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -4,3 +4,37 @@ const nav = document.querySelector('nav');
 menu.addEventListener('click', () => {
     nav.classList.toggle('toggle-nav');
 });
+
+class Tabs {
+    constructor(container) {
+        this.container = container;
+        this.tabs = container.querySelectorAll('.trigger')
+    }
+    init() {
+        this.tabs.forEach(tab => {
+            tab.addEventListener('click', (e) => {
+                this.toggleTabs(e);
+                this.toggleContent(e);
+            })
+        })
+    }
+    toggleTabs(e) {
+        // remove current active clases
+        this.tabs.forEach(tab => tab.classList.remove('active'));
+        // add new active class to clicked tab
+        e.target.classList.add('active');
+    }
+    toggleContent(e) {
+        // remove current active classes from content
+        this.container.querySelectorAll('.content').forEach(itemt => {
+            itemt.classList.remove('active');
+        });
+        // add new active class to content
+        const selector = e.target.getAttribute('data-target');
+        const content = this.container.querySelector(selector);
+        content.classList.add('active');
+    }
+}
+
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
