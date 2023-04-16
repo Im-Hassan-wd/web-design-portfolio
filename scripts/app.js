8d6699ef7b982e4e9b36c1c29079b6b4141b2692
Im-Hassan-wd <alhassansahih@gmail.com>
2023-04-16 13:01:37 +0100

    removed the console log

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,45 @@
-const menu = document.querySelector('.menu');
-const nav = document.querySelector('nav');
-const social = document.querySelector('.social');
-const workSection = document.querySelector('.my-work');
-const projects = document.querySelectorAll('.work');
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+const social = document.querySelector(".social");
+const workSection = document.querySelector(".my-work");
+const projects = document.querySelectorAll(".work");
 const previewPopup = document.querySelector(".preview-container");
 const previewWrap = document.querySelector(".preview-wrap");
-const closePopup =  document.querySelector('.close-popup button')
+const closePopup = document.querySelector(".close-popup button");
 
-menu.addEventListener('click', () => {
-    nav.classList.toggle('toggle-nav');
-    social.classList.toggle('active');
+menu.addEventListener("click", () => {
+  nav.classList.toggle("toggle-nav");
+  social.classList.toggle("active");
 });
 
-window.addEventListener('scroll', bgTransition)
+window.addEventListener("scroll", bgTransition);
 
 function bgTransition() {
-    const sectionTop = workSection.getBoundingClientRect().top;
-    if(sectionTop < 24) {
-        workSection.classList.add('transition-bg')
-    }else {
-        workSection.classList.remove('transition-bg')
-    }
+  const sectionTop = workSection.getBoundingClientRect().top;
+  if (sectionTop < 24) {
+    workSection.classList.add("transition-bg");
+  } else {
+    workSection.classList.remove("transition-bg");
+  }
 }
 
 const previewProject = () => {
-   projects.forEach((project, i) => {
-    project.addEventListener('click', function () {
-        const dataCode = this.children[0].getAttribute('data-code');
-        const dataDemo = this.children[0].getAttribute('data-demo');
-        previewWrap.children[1].children[0].href = dataCode;
-        previewWrap.children[1].children[1].href = dataDemo;
-        console.log(dataCode, dataDemo)
+  projects.forEach((project, i) => {
+    project.addEventListener("click", function () {
+      const dataCode = this.children[0].getAttribute("data-code");
+      const dataDemo = this.children[0].getAttribute("data-demo");
+      previewWrap.children[1].children[0].href = dataCode;
+      previewWrap.children[1].children[1].href = dataDemo;
 
-        previewWrap.classList.add('active');
-        previewPopup.classList.add('active');
-    })
-   }) 
-}
+      previewWrap.classList.add("active");
+      previewPopup.classList.add("active");
+    });
+  });
+};
 
-closePopup.addEventListener('click', () => {
-    previewWrap.classList.remove('active');
-    previewPopup.classList.remove('active'); 
-})
+closePopup.addEventListener("click", () => {
+  previewWrap.classList.remove("active");
+  previewPopup.classList.remove("active");
+});
 
 previewProject();
